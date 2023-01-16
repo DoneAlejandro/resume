c883420ed0fa8531619511d836248304009282d8
Alejandro <DonAlejandroCarlos@yandex.ru>
2023-01-16 15:21:33 +0300

    new module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import style from './Header.module.scss'
 
 export const Header = () => {
   return (
-    <header className="container">
+    <header className={style.container}>
       <div className="header">
         <h1 className="header__title">
           <NavLink to={"/"}>Болдырев Алексей</NavLink>
         </h1>
-		<div className="header__navbar">
-			<NavLink></NavLink>
-		</div>
+        <div className="header__navbar">
+          <NavLink to={"/projects"}>Мои проекты</NavLink>
+          <NavLink to={"/about"}>Обо мне</NavLink>
+          <NavLink to={"/skills"}>Навыки</NavLink>
+          <NavLink to={"/contact"}>Контакты</NavLink>
+        </div>
       </div>
     </header>
   );
