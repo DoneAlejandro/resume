707db430bd8038c6c3bed3c2433fe49d18742dd9
Alejandro <DonAlejandroCarlos@yandex.ru>
2023-01-19 17:32:55 +0300

    added mobile menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.scss';
-import { Header } from './components/Header'
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.scss";
+import { Header } from "./components/Header";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { About } from "./pages/About/About";
+import { Projects } from "./pages/Projects/Projects";
+import { Skills } from "./pages/Skills/Skills";
+import { Contact } from "./pages/Contact/Contact";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-	<Header />
-	
+      <Header />
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about-me" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import style from './Header.module.scss'
+import style from "./Header.module.scss";
 
 export const Header = () => {
+  const [active, setActive] = React.useState(true);
+  const toggleClass = () => {
+    setActive(!active);
+  };
   return (
     <header className={style.container}>
-      <div className="header">
-        <h1 className="header__title">
+      <div className={style.header}>
+        <h1 className={style.header__title}>
           <NavLink to={"/"}>Болдырев Алексей</NavLink>
         </h1>
-        <div className="header__navbar">
-          <NavLink to={"/projects"}>Мои проекты</NavLink>
-          <NavLink to={"/about"}>Обо мне</NavLink>
-          <NavLink to={"/skills"}>Навыки</NavLink>
-          <NavLink to={"/contact"}>Контакты</NavLink>
+        <div className={active ? `${style.header__navbar}` : `${style.header__navbar} ${style._active}`}>
+          <NavLink className={style.header__link} to={"/projects"}>
+            Мои проекты
+          </NavLink>
+          <NavLink className={style.header__link} to={"/about"}>
+            Обо мне
+          </NavLink>
+          <NavLink className={style.header__link} to={"/skills"}>
+            Навыки
+          </NavLink>
+          <NavLink className={style.header__link} to={"/contact"}>
+            Контакты
+          </NavLink>
+        </div>
+        <div className={active ? `${style.header__icon}` : `${style.header__icon} ${style._active}`} onClick={() => toggleClass()}>
+          <span></span>
         </div>
       </div>
     </header>
