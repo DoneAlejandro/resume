01f7e76a6bfea6aa1e63aac29be3a287784e04b3
Alejandro <DonAlejandroCarlos@yandex.ru>
2023-01-30 17:29:21 +0300

    new page

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import style from "./About.module.scss";
+import avatar from "../../../assets/images/avatar-about.png";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={style.about}>
+      <div className={style.about__title}>
+        <h1>Обо мне</h1>
+      </div>
+      <div className={style.about__wrapper}>
+        <div className={style.about__description}>
+          <span>Меня зовут Болдырев Алексей. Мне 26 лет. Полтора года назад я решил изменить свою жизнь и решил заняться программированием. Начинал с языка программирования Python. На нём я написал парочку ботов для телеграм. И однажды я наткнулся на обучающие ролики по веб программированию. Таким образом я нашёл то, что мне безумно нравится и то, что у меня вызывает детский восторг как когда собираешь конструктор лего</span>
+        </div>
+        <div className={style.about__avatar}>
+          <img src={avatar} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
